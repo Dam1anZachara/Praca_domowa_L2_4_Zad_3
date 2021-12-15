6f87dc11cc3f1af66142d4a3eca0007a99309ebf
damia <damia@DESKTOP-G1NUIKK>
2021-12-15 22:27:56 +0100

    Add project files.

---
+++ L2_4_Zad_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L2_4_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.\r\nThis program calculates the diagonal length \r\n" +
+                "of the rectangle based on the given side lengths.");
+            Console.Write("Please enter the length of the first side: ");
+            bool firstSideCanBeDouble = double.TryParse(Console.ReadLine(), out double firstSideDouble);
+            Console.Write("Please enete the length of the second side: ");
+            bool secondSideCanBeDouble = double.TryParse(Console.ReadLine(), out double secondSideDouble);
+
+            if (firstSideCanBeDouble && secondSideCanBeDouble)
+            {
+                double diagonalOfTheRectangle = Math.Sqrt(Math.Pow(firstSideDouble, 2) + Math.Pow(secondSideDouble, 2));
+                Console.WriteLine($"Calculated diagonal length is: {diagonalOfTheRectangle}");
+            }
+            else
+            {
+                Console.WriteLine("You enterd wrong type length values of rectangle sides! Try Again.");
+            }
+        }
+    }
+}
